761f62148395e819c4a33c41793c7264a3abe84a
Cindy Chen <cindyjan15@gmail.com>
2017-07-21 16:46:45 -0700

    new changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,9 +3,7 @@ var friends = require("../data/friends.js");
 module.exports = function(app) {
 
 	app.get("/api/friends", function(req, res) {
-
         res.json(friends);
-
     });
 
     app.post("/api/friends", function(req, res) {
@@ -23,42 +21,24 @@ module.exports = function(app) {
             var matchScoreInt = friends[i]['score[]'].map(function(num) {
                 return parseInt(num);
             });
-
             for (var j = 0; j < scoreInt.length; j++) {
-
                 var diff = scoreInt[j] - matchScoreInt[j];
-
                 if (diff < 0) {
-
                     diff = diff * -1;
-
                 }
-
                 diffArray.push(diff);
-
             }
 
             var totalDiff = diffArray.reduce(function(sum, value) {
                 return sum + value;
             });
-
             if (totalDiff <= smallestDiff) {
-
                 smallestDiff = totalDiff;
-
                 bestMatch = friends[i];
-
             }
-
-
             diffArray = [];
-
         }
-
         res.json(bestMatch);
-
         friends.push(req.body);
-
     });
-
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,6 @@ module.exports = function(app) {
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-
     app.use(function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
